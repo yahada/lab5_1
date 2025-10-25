b030cf5ba9504e137d2ea5af0b2a2fc11b46f68d
yahada <niadzy14@gmail.com>
2025-10-25 12:36:09 +0300

    input one u_i

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
 int main()
 {
-    
+  size_t a = 0;
+  std::cin >> a;
+
+
+  if (std::cin.eof())
+  {
+    std::cout << 0;
+    std::cout << "\n";
+
+  } else if (std::cin.fail()){
+    std::cerr << "Error\n";
+    return 1;
+  }
 }
