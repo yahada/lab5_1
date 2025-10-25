5c638a3dedac350394e7d3b6290c2e60f8bac5c3
yahada <niadzy14@gmail.com>
2025-10-25 13:09:38 +0300

    isPyth

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+
+bool isPyth(size_t a, size_t b, size_t c)
+{
+  bool p = (a*a == (b*b+c*c));
+  p = p || (b*b == (a*a+c*c));
+  p = p || (c*c == (a*a+b*b));
+  return p;
+
+}
+
 int main()
 {
-  size_t a = 0;
+  size_t a = 0, b = 0, c = 0;
   size_t count = 0;
   while (std::cin >> a)
   {
-    count+=a*a;
+    count+=isPyth(a,b,c) ? 1:0;
+    c = b;
+    b = a;
+
   }
 
   if (std::cin.eof())
