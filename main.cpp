946b148bb299a47336629d4c42b2b246670cb689
yahada <niadzy14@gmail.com>
2025-10-25 12:46:56 +0300

    sequence process

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 int main()
 {
   size_t a = 0;
-  std::cin >> a;
-
+  size_t count = 0;
+  while (std::cin >> a)
+  {
+    count++;
+  }
 
   if (std::cin.eof())
   {
-    std::cout << 0;
+    std::cout << count;
     std::cout << "\n";
 
   } else if (std::cin.fail()){
